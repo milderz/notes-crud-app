17b8a37bb03ab3f7fc59bae0e911e688b54c5a32
milderz <75046268+milderz@users.noreply.github.com>
2022-11-19 12:40:02 -0600

    Created env variables

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdGi1B0owA3agY9mLHr2PU4dQzfTJbx5o",
-  authDomain: "notes-app-dbb27.firebaseapp.com",
-  projectId: "notes-app-dbb27",
-  storageBucket: "notes-app-dbb27.appspot.com",
-  messagingSenderId: "526465418147",
-  appId: "1:526465418147:web:3a7c000fa3cb5ebd835016",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 export const app = initializeApp(firebaseConfig);
